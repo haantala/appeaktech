82fd7dc8e9057c8c8281cd1cb0fa6b4e10ddbfb9
Harsh Antala <h.desai@techier.dev>
2025-05-24 17:23:20 +0530

    error solved

--- React/src/@core/api/index.tsx
+++ React/src/@core/api/index.tsx
@@ -1,21 +1,10 @@
+/* eslint-disable no-constant-binary-expression */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // import { handleToast } from "@/utils/toast";
 import type { AxiosResponse } from "axios";
 import Axios from "axios";
 
 import { ApiRoutes } from "../constants";
-
-const getToken = () => {
-  if (typeof window !== "undefined") {
-    try {
-      return localStorage.getItem("token");
-    } catch {
-      return null;
-    }
-  }
-  // If not in a browser environment, return null or handle accordingly
-  return null;
-};
 
 export const axios = Axios.create({
   baseURL: ApiRoutes.API_HOSTNAME,
@@ -27,14 +16,12 @@ const endPoint = ["uploadmedia"];
 
 axios.interceptors.request.use(
   async (config: any) => {
-    const token = getToken();
     config.headers = {
       Accept: "application/json , */*",
       "Content-Type": endPoint.includes(config.url)
         ? "multipart/form-data"
         : "application/json",
-      Authorization: `Bearer ${token} || "{}"
-      `,
+      Authorization: `Bearer ${localStorage.getItem("token")}` || "",
     };
 
     return config;
